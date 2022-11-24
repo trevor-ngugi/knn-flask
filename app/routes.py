2cb203a850f8e67f25e3a2ffb853545b187a54a9
trevor ngugi <ngugitrevor@gmail.com>
2022-11-24 15:32:38 +0300

    added gender functionality

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 
 model=pickle.load(open('model.pkl','rb')) 
+modeltwo=pickle.load(open('modelfix.pkl','rb')) 
 @app.route('/')
 @app.route('/home')
 def home():
@@ -13,6 +14,19 @@ def home():
 def index():
     return render_template('index.html', title='Home',data='hey')
 
+@app.route('/aisha')
+def trial():
+    return render_template('aisha.html', title='with gender',data='hey')
+
+@app.route("/predictiontwo",methods=["POST"])
+def predictiontwo():
+    height=float(request.form['height'])
+    weight=float(request.form['weight'])
+    gender=float(request.form['gender'])
+    arr=np.array([[height,weight,gender]])
+    pred=modeltwo.predict(arr)
+    return render_template('aishapred.html',data=pred)
+
 @app.route("/prediction",methods=["POST"])
 def prediction():
     height=float(request.form['height'])
@@ -21,9 +35,7 @@ def prediction():
     pred=model.predict(arr)
     return render_template('prediction.html',data=pred)
 
-@app.route("/aisha")
-def aisha():
-    return render_template('test.html', title='aisha code')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
